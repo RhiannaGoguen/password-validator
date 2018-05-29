c3e0b71a90eacda3b2a5868294df8416d9b07459
Rhianna Goguen <rhianna.goguen@dal.ca>
2018-05-29 16:29:40 -0300

    Adding a few more tests for the two rules, checking edge cases. Added an empty validate function that just returns -1. Updated the tests to include use of the validate function. Now can begin writing validate function code.

--- app/src/main/java/com/example/rhianna/password_validator/Validator.java
+++ app/src/main/java/com/example/rhianna/password_validator/Validator.java
@@ -2,6 +2,12 @@ package com.example.rhianna.password_validator;
 
 public class Validator {
 
+    public Validator(){
 
+    }
+
+    public static int validate(String pass){
+        return -1;
+    }
 
 }
--- app/src/test/java/com/example/rhianna/password_validator/ValidatorTest.java
+++ app/src/test/java/com/example/rhianna/password_validator/ValidatorTest.java
@@ -1,17 +1,54 @@
 package com.example.rhianna.password_validator;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static junit.framework.Assert.*;
 
 public class ValidatorTest {
 
+    static Validator validator;
+    String pass = "";
+
+    @BeforeClass
+    public static void setUp(){
+        validator = new Validator();
+    }
+
     @Test
-    public void isNotPassword(){
-        fail("Not implemented yet");
+    public void isNotPasswordPass(){
+        pass = "notTheWordPassword";
+        assertEquals(validator.validate(pass), 2);
+    }
+
+    @Test
+    public void isNotPasswordFail(){
+        pass = "password";
+        assertEquals(validator.validate(pass), 1);
+    }
+
+    @Test
+    public void isNotPasswordFailCaseDifferences(){
+        pass = "PassWOrd";
+        assertEquals(validator.validate(pass), 1);
     }
 
     @Test
     public void atLeast8CharLong(){
-        fail("Not implemented yet");
+        pass = "OverEightCharacters";
+        assertEquals(validator.validate(pass), 2);
+    }
+
+    @Test
+    public void exactly8CharLong(){
+        pass = "12345678";
+        assertEquals(validator.validate(pass), 2);
+    }
+
+    @Test
+    public void lessThan8CharLong(){
+        pass = "aaa";
+        assertEquals(validator.validate(pass), 1);
     }
 
 }
