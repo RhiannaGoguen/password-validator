6248e6014b72858452d30f0f8f14935d4ad098aa
Rhianna Goguen <rhianna.goguen@dal.ca>
2018-05-29 16:35:24 -0300

    Implemented validate method. Tests for current two rules now all pass

--- app/src/main/java/com/example/rhianna/password_validator/Validator.java
+++ app/src/main/java/com/example/rhianna/password_validator/Validator.java
@@ -2,12 +2,21 @@ package com.example.rhianna.password_validator;
 
 public class Validator {
 
-    public Validator(){
-
-    }
+    public Validator(){}
 
     public static int validate(String pass){
-        return -1;
+        int rulesPassed = 0;
+
+        //Rule 1: The password is not the word "password".
+        if(!pass.equalsIgnoreCase("password"))
+            rulesPassed++;
+
+        //Rule 2: The password is at least 8 characters long.
+        if(pass.length()>=8){
+            rulesPassed++;
+        }
+
+        return rulesPassed;
     }
 
 }
