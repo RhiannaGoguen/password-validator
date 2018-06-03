183ec65ceaea29445a0912a7d21ec21f58b22ffb
Rhianna Goguen <rhianna.goguen@dal.ca>
2018-06-03 15:04:20 -0300

    Updated gradle for espresso tests, made a UI_Test class for the espresso tests

---
+++ app/src/androidTest/java/com/example/rhianna/password_validator/UI_Test.java
@@ -0,0 +1,12 @@
+package com.example.rhianna.password_validator;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class UI_Test {
+
+
+}
